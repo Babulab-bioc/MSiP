f03d9448171e642289b03073db21ff7b12eea5df
Matineh Rahmatbakhsh <matinerb.94@gmail.com>
2021-06-17 23:13:05 -0600

    Delete R directory

--- R/testdfClassifier.R
+++
@@ -1,9 +0,0 @@
-#' Test data for classifier
-#'
-#' @description Scored data.frame
-#' @name testdfClassifier
-#' @usage data(testdfClassifier)
-#' @docType data
-#' @author Matineh Rahmatbakhsh, \email{matinerb.94@gmail.com}
-#' @keywords datasets
-NULL
